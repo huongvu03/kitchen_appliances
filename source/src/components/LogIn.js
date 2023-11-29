c5f4f569b66a2464bc5a0a99fe06ff961ff4b5f2
Trâm Nguyễn <151357844+TramNguyen1428@users.noreply.github.com>
2023-11-29 20:11:53 +0700

    Update LogIn.js

--- source/src/components/LogIn.js
+++ source/src/components/LogIn.js
@@ -1,6 +1,36 @@
-function Login(){
-    return(
-        <div>Trang login</div>
-    );
-}
-export default Login;
+import { useState } from "react";
+    
+    function Login({checkLogin, errorLogin}){
+        const [username, setUsername] = useState('');
+        const [password, setPassword] = useState('');
+        const handleLogin = (e) => {
+            e.preventDefault();
+            const checkUser = {username, password};
+            checkLogin(checkUser);
+            setUsername('');
+            setPassword('');
+        }
+    
+        return(
+            <div>
+            <form onSubmit={handleLogin}>
+                <h4>{errorLogin}</h4>
+                <table>
+                    <tr>
+                        <td>Username</td>
+                        <td><input type="text" value={username} 
+                            onChange={(e) => setUsername(e.target.value)}/></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" value={password} 
+                            onChange={(e) => setPassword(e.target.value)}/></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2"><input type="submit" value="Login"/></td>
+                    </tr>
+                </table>
+            </form>
+            </div>);
+    }
+    export default Login;
