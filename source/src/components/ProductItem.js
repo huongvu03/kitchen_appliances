a030c2bc07b5239cea4420368bb680bcd88523ab
hoang765218 <mvhoang765218@gmail.com>
2023-12-08 14:10:11 +0700

    Update ProductItem.js

--- source/src/components/ProductItem.js
+++ source/src/components/ProductItem.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "../css/ProductItem.css";
-import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
-import 'sweetalert2/dist/sweetalert2.min.css';
+// import Swal from 'sweetalert2';
+// import 'sweetalert2/dist/sweetalert2.min.css';
 function ProductItem({ product, addToCart }) {
-    const showAlert=()=>{
-        Swal.fire({
-            text: 'S U C C E S S !',
-        })
-    };
+    // const showAlert = () => {
+    //     Swal.fire({
+    //         text: 'S U C C E S S !',
+    //     })
+    // };
     const navigate = useNavigate();
     return (
         <div className="productitem">
@@ -20,7 +20,7 @@ function ProductItem({ product, addToCart }) {
                         <div>${product.price}</div></div>
                 </div>
                 <div className="productitem_card_control">
-                    <button className='btn_addCart' onClick={() => {addToCart(product);showAlert()}}>ADD TO CART</button>
+                    <button className='btn_addCart' onClick={() => { addToCart(product) }}>ADD TO CART</button>
                     <button className='btn_detail' onClick={() => navigate(`/detail/${product.id}`)}>DETAILS</button>
                 </div>
             </div>
