656735e9ab0ad79b412c532e8fe71b2bffa8dec1
hoang765218 <mvhoang765218@gmail.com>
2023-12-13 19:39:24 +0700

    e css navbar , voi css mau may cai button

--- source/src/components/Header.js
+++ source/src/components/Header.js
@@ -16,23 +16,23 @@ function Header({ checkHeader, carts }) {
         <div className="headerHome">
             <div>  <Link to='/'><img src="./homeimg/Logo-Tashas-Design.png" alt="logo" height="100px" width="100px" /></Link></div>
             <ul>
-                <Link to='/'> <li>HOME</li></Link>
-                <Link to='/promotion'> <li>PROMOTION</li></Link>
+                <Link to='/' className="navbar-link"> <li>HOME</li></Link>
+                <Link to='/promotion' className="navbar-link"> <li>PROMOTION</li></Link>
 
-                <Link to='/products'><li>PRODUCTS</li></Link>
-                <Link to='/blogs'> <li>OUR BLOG </li></Link>
-                <Link to='/contact'> <li>
-                    <div class="headerHome_dropdown">
-                        <div className='dropbtn'>SUPPORT</div>
+                <Link to='/products' className="navbar-link"><li>PRODUCTS</li></Link>
+                <Link to='/blogs' className="navbar-link"> <li>OUR BLOG </li></Link>
+                <Link to='/contact' className="navbar-link">SUPPORT <li>
+                    {/* <div class="headerHome_dropdown">
+                        <div className='dropbtn navbar-link'>SUPPORT</div>
                         <div class="dropdown-content">
                             <a href="/manuals">Manuals</a>
                             <a href="/feedback">Feedback</a>
                             <a href="/register-complain">Register Complain</a>
                         </div>
-                    </div>
+                    </div> */}
 
                 </li></Link>
-                <Link to='/about-us'><li>ABOUT US</li></Link>
+                <Link to='/about-us' className="navbar-link"><li>ABOUT US</li></Link>
             </ul>
             <div></div>
             <div className='headerHome4'>
