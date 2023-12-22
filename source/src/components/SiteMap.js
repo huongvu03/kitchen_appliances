7a895d1fcc757dd1df96b9054b24a3d072dca7ac
chidoan123 <chidoan85@gmail.com>
2023-12-22 14:13:05 +0700

    sitemap

---
+++ source/src/components/SiteMap.js
@@ -0,0 +1,12 @@
+import '../css/Footer.css'
+
+function SiteMap(){
+    return (
+
+        <div className='Sitemap'>
+          <div><img src="./homeimg/sitemap.jpg" alt="sitemap"/></div>  
+        </div>
+       
+    );
+}
+export default SiteMap;
